bfbefb398b9794b139b086902b1b71eeeb466f55
KESIA DE SOUZA GONZAGA <07383764188@ifms.edu.br>
2023-08-15 07:33:58 -0400

    first commit

---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,32 @@
+import './Header.css'
+function Header(){
+        return(
+            <header>
+              <div>
+                Logo
+              </div>
+              <nav>
+                <ul>
+                    <li>
+                        <a href="#">home</a>
+                    </li>
+
+                    <li>
+                        <a href="#">sobre</a>
+                    </li>  
+
+                    <li>
+                     <a href="#">noticia</a>
+                    </li>
+                    
+                       <li>
+                        <a href="#">contato</a>
+                       </li>
+                        
+                   
+                </ul>
+              </nav>
+            </header>
+        )
+}
+export default Header
