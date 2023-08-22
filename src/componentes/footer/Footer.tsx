b7e9fce8fb793af3ddbd3bd960cf53d25808e1ac
kesiagonzzaga05 <kesia.gonzaga@estudante.ifms.edu.br>
2023-08-22 08:05:55 -0400

    iniciando a programação do footer

---
+++ src/componentes/footer/Footer.tsx
@@ -0,0 +1,10 @@
+ import './Footer.css'
+ import Logo from '../../assets/b.png'
+export default function Footer(){
+    return(
+        <footer>
+        <img src={Logo} alt=""/>
+        </footer>
+    )
+}
+
